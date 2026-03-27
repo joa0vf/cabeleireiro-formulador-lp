947396c317f24481f061d650ce33fa9e349def27
joa0vf <ioaovictordesign@gmail.com>
2026-03-27 12:19:25 -0300

    fix: re-adicionando script identico ao fornecido pelo usuario

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,34 +37,32 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR" className={`${inter.variable} ${arima.variable}`}>
-      <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1946553129283243');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-      </head>
       <body suppressHydrationWarning className="font-sans antialiased bg-[#FDFBF7] text-[#1A1A1A] overflow-x-hidden">
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1946553129283243');
+fbq('track', 'PageView');
+          `}
+        </Script>
         <noscript>
-          <img
-            height="1"
-            width="1"
+          <img 
+            height="1" 
+            width="1" 
             style={{ display: 'none' }}
             src="https://www.facebook.com/tr?id=1946553129283243&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
+        {/* End Meta Pixel Code */}
         {children}
       </body>
     </html>
